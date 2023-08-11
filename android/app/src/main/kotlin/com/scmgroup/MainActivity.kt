01b3c8dc52ba0e4de04e5b2ea452e823fac9c038
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2023-08-11 09:44:21 +0200

    feat(chore): :fire: default template

---
+++ android/app/src/main/kotlin/com/scmgroup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gellify.flutter_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/shared/style/app_icons.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: non_constant_identifier_names
+
+class AppIcons {
+  static final visibilityOn = _iconPath('visibility_on');
+  static final visibilityOff = _iconPath('visibility_off');
+
+  static String _iconPath(String iconName) => 'assets/icons/$iconName.svg';
+}
